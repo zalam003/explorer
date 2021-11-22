f865bca87edc83b9044dd3ca8d52cbfeeeeaa3df
zalam003 <zaki.alam@gmail.com>
2021-11-22 12:48:07 -0500

    remove moniker

---
+++ scripts/remove_moniker.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+#
+# Description: Remove '@ Energi' moniker from token names
+#
+#set -x
+
+if [[ $1 == mainnet ]]
+then
+    . ./mainnet.env
+    export TMPFILE=token_moniker_mainnet.txt
+
+elif [[ $1 == testnet ]]
+then
+    . ./testnet.env
+    export TMPFILE=token_moniker_testnet.txt
+
+elif [[ $1 == develop ]]
+then
+    . ./develop.env
+    export TMPFILE=token_moniker_mainnet.txt
+
+elif [[ $1 == local ]]
+then
+    . ./local.env
+    export TMPFILE=token_moniker_mainnet.txt
+
+else
+    echo
+    echo "Usage: $0 <environment>"
+    echo "    environments are:"
+    echo "        local    - Local"
+    echo "        develop  - Develop"
+    echo "        testnet  - Testnet"
+    echo "        mainnet  - Mainnet"
+    echo
+    exit
+
+fi
+
+# export variables
+export PGPORT=5432
+export PGHOST
+export PGDATABASE
+export PGUSER
+export PGPASSWORD
+
+# Install psql client if not there
+if [ ! -x "$( command -v psql )" ]
+then
+    sudo apt-get update
+    sudo apt -y install postgresql-client
+
+fi
+
+echo "Download tokens with moniker"
+psql -d $PGDATABASE -U $PGUSER -h $PGHOST -t -c "SELECT name,contract_address_hash FROM tokens WHERE name LIKE '%@ Energi';" > ${TMPFILE}
+
+if [[ ! -s ${TMPFILE} ]]
+then
+    #######
+    # Rename Tokens
+    #######
+    IFS=$'\n'
+    for LINE in $( cat ${TMPFILE} )
+    do
+        CONTRACT=$(echo "$LINE" | awk -F\| '{print $2}' | awk '{$1=$1};1')
+        NAME=$(echo "$LINE" | awk -F\| '{print $1}' | sed 's/ @ Energi//g' | awk '{$1=$1};1' )
+        echo "Rename Token: ${NAME} with contract ${CONTRACT}"
+        psql -d $PGDATABASE -U $PGUSER -h $PGHOST -t -c "UPDATE tokens SET
+          name = '${NAME}'
+          WHERE contract_address_hash = '${CONTRACT}';"
+    done
+
+else
+    echo "Nothing to update..."
+
+fi
+
+# Clean up
+rm ${TMPFILE}
+

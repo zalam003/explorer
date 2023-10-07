c0ab49d95133668ef3f733f57e2943923d09f838
zalam003 <zaki.alam@gmail.com>
2023-10-07 14:50:39 -0400

    revert source.ex

--- apps/explorer/lib/explorer/exchange_rates/source.ex
+++ apps/explorer/lib/explorer/exchange_rates/source.ex
@@ -7,28 +7,14 @@ defmodule Explorer.ExchangeRates.Source do
   alias Explorer.ExchangeRates.Source.CoinGecko
   alias Explorer.ExchangeRates.Token
   alias HTTPoison.{Error, Response}
-  alias Explorer.Chain
 
   @doc """
   Fetches exchange rates for currencies/tokens.
   """
   @spec fetch_exchange_rates(module) :: {:ok, [Token.t()]} | {:error, any}
   def fetch_exchange_rates(source \\ exchange_rates_source()) do
-    wnrg_token_address = Application.get_env(:explorer, :wnrg_token_address)
     source_url = source.source_url()
-    nrg_coin_details = fetch_exchange_rates_request(source, source_url, source.headers())
-    wnrg_price = fetch_token_price(wnrg_token_address)
-
-    if(is_nil(wnrg_price)) do
-      nrg_coin_details
-    else
-      {:ok, [nrg_details_map]} = nrg_coin_details
-      wnrg_price =
-        fetch_token_price(wnrg_token_address)
-        |> Decimal.round(4)
-      # Update usd_value of the map(fetched from Coingecko) with the price fecthed from energiswap
-      {:ok, [Map.put(nrg_details_map, :usd_value, wnrg_price)]}
-    end
+    fetch_exchange_rates_request(source, source_url, source.headers())
   end
 
   @spec fetch_exchange_rates_for_token(String.t()) :: {:ok, [Token.t()]} | {:error, any}
@@ -45,75 +31,14 @@ defmodule Explorer.ExchangeRates.Source do
     fetch_exchange_rates_request(CoinGecko, source_url, headers)
   end
 
-  @spec fetch_energiswap_exchange_rates_for_tokens() :: [any]
-  def fetch_energiswap_exchange_rates_for_tokens() do
-    energiswap_api_url = Application.get_env(:explorer, :energiswap_api_url)
-
-    if(is_nil(energiswap_api_url)) do
-      nil
-    else
-      {:ok, body} = http_request(energiswap_assets_url(), energiswap_headers())
-      {:ok, result} = parse_http_success_response(body)
-      result
-    end
+  @spec fetch_market_data_for_token_addresses([Hash.Address.t()]) ::
+          {:ok, %{Hash.Address.t() => %{fiat_value: float() | nil, circulating_market_cap: float() | nil}}}
+          | {:error, any}
+  def fetch_market_data_for_token_addresses(address_hashes) do
+    source_url = CoinGecko.source_url(address_hashes)
+    headers = CoinGecko.headers()
+    fetch_exchange_rates_request(CoinGecko, source_url, headers)
   end
-
-  def energiswap_base_url() do
-    Application.get_env(:explorer, :energiswap_api_url)
-  end
-
-  def energiswap_assets_url() do
-    "#{energiswap_base_url()}/assets"
-  end
-
-  def energiswap_lp_url() do
-    "#{energiswap_base_url()}/lpprices"
-  end
-
-  @spec fetch_energiswap_exchange_rates_for_lp_tokens() :: [any]
-  def fetch_energiswap_exchange_rates_for_lp_tokens() do
-
-    lp_url = energiswap_lp_url()
-
-    if(is_nil(lp_url)) do
-      nil
-    else
-      {:ok, body} = http_request(lp_url, energiswap_headers())
-      {:ok, result} = parse_http_success_response(body)
-      format_lp_tokens(result)
-    end
-  end
-
-  @spec fetch_token_price(String.t()) :: [any]
-  def fetch_token_price(token_address_str) do
-    result = fetch_energiswap_exchange_rates_for_tokens()
-    parse_token_price(result, token_address_str)
-  end
-
-  def parse_token_price(result, address_hash) do
-    if(result[address_hash]) do
-      to_decimal(result[address_hash]["last_price"])
-    else
-      nil
-    end
-  end
-
-  def format_lp_tokens(tokens) do
-    tokens =
-      Enum.map(tokens, fn token ->
-        {:ok, address_hash} = Chain.string_to_address_hash(token["id"])
-        checksumed_address = Chain.Address.checksum(address_hash)
-        %{
-          checksumed_address => %{
-          "symbol" => "#{token["token0"]["symbol"]}/#{token["token1"]["symbol"]}",
-          "last_price" => token["lpPriceUSD"],
-          }
-        }
-      end)
-    for token <- tokens, {address, details} <- token, into: %{}, do: {address, details}
-  end
-
-  defp fetch_exchange_rates_request(_source, source_url) when is_nil(source_url), do: {:error, "Source URL is nil"}
 
   @spec fetch_token_hashes_with_market_data :: {:ok, [String.t()]} | {:error, any}
   def fetch_token_hashes_with_market_data do
@@ -166,11 +91,6 @@ defmodule Explorer.ExchangeRates.Source do
     [{"Content-Type", "application/json"}]
   end
 
-  def energiswap_headers do
-    energiswap_auth_secret = Application.get_env(:explorer, :energiswap_auth_secret)
-    [{"authorization_secret", energiswap_auth_secret}]
-  end
-
   def decode_json(data) do
     Jason.decode!(data)
   rescue
@@ -199,8 +119,8 @@ defmodule Explorer.ExchangeRates.Source do
     Application.get_env(:explorer, __MODULE__, [])[key]
   end
 
-  def http_request(source_url, headers \\ headers()) do
-    case HTTPoison.get(source_url, headers) do
+  def http_request(source_url, additional_headers) do
+    case HTTPoison.get(source_url, headers() ++ additional_headers) do
       {:ok, %Response{body: body, status_code: 200}} ->
         parse_http_success_response(body)
 

0ade96541cc3f79b245b134259c37e19ef3e4b01
zalam003 <zaki.alam@gmail.com>
2023-10-10 15:13:47 -0400

    rebase genesis_data.ex

--- scripts/genesis_data-energi.ex
+++ scripts/genesis_data-energi.ex
@@ -84,7 +84,6 @@ defmodule Explorer.ChainSpec.GenesisData do
             end
 
           {:error, reason} ->
-            # credo:disable-for-next-line
             Logger.warn(fn -> "Failed to fetch genesis data. #{inspect(reason)}" end)
         end
       end)
@@ -103,8 +102,9 @@ defmodule Explorer.ChainSpec.GenesisData do
 
   # sobelow_skip ["Traversal"]
   defp fetch_from_file(path) do
-    with {:ok, data} <- File.read(path) do
-      Jason.decode(data)
+    with {:ok, data} <- File.read(path),
+         {:ok, json} <- Jason.decode(data) do
+      {:ok, json}
     end
   end
 
